1e3225168fdc385d073a6631dd7ed80656bb41ce
Adam McLain <pdxadam@gmail.com>
2024-01-09 21:01:18 -0800

    Connect to database. Check hashed userpassword

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.18
 
 require (
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"embed"
 	"fmt"
 	"html/template"
@@ -15,6 +16,7 @@ import (
 	"traxl/pkg"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 
 	_ "github.com/lib/pq"
 )
@@ -29,7 +31,9 @@ var (
 	//go:embed tmpl/*.html
 	tmplEmbed embed.FS
 
-	dbQuery dbTraxl.Queries
+	dbQuery *dbTraxl.Queries
+
+	store = sessions.NewCookieStore([]byte("forDemo"))
 )
 
 type staticHandler struct {
@@ -53,6 +57,7 @@ func (h staticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 }
 func renderFiles(tmpl string, w http.ResponseWriter, d interface{}) {
+	log.Println("Rendering " + tmpl)
 	t, err := template.ParseFS(tmplEmbed, fmt.Sprintf("tmpl/%s.html", tmpl))
 	if err != nil {
 		log.Fatal(err)
@@ -62,39 +67,67 @@ func renderFiles(tmpl string, w http.ResponseWriter, d interface{}) {
 	}
 }
 
-// func postHandler(w http.ResponseWriter, r *http.Request) {
-// 	result := myResult{Message: "Success", SecondPart: "You Made it"}
-// 	r.ParseForm()
-// 	if validateUser(r.FormValue("username"), r.FormValue("password")) {
-// 		result.Message = "The login was successful"
-// 	} else {
-// 		result.Message = "Unsuccesful"
-// 		result.SecondPart = "Please Try again"
-// 	}
-// 	t, err := template.ParseFiles("static/tmpl/loginResult.html")
-// 	if err != nil {
-// 		fmt.Fprintf(w, "error processing")
-// 		return
-// 	}
-// 	tpl := template.Must(t, err)
-// 	tpl.Execute(w, result)
+func securityMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if sessionValid(w, r) {
+			//login path will be let through.
+			if r.URL.Path == "/login" {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
 
-// }
+		if hasBeenAuthenticated(w, r) {
+			next.ServeHTTP(w, r)
+			return
+		}
+		//otherwise redirect to login
+		storeAuthenticated(w, r, false)
+		http.Redirect(w, r, "/login", 307)
+
+	})
+}
+func sessionValid(w http.ResponseWriter, r *http.Request) bool {
+	session, _ := store.Get(r, "session_token")
+	return !session.IsNew
+
+}
 func authenticationHandler(w http.ResponseWriter, r *http.Request) {
 	result := "Login "
 	r.ParseForm()
-
+	log.Println(r.FormValue("username"))
+	log.Println(r.FormValue("password"))
 	if validateUser(r.FormValue("username"), r.FormValue("password")) {
 		//TODO: do the store authenticated part
 		result = result + "successful"
 	} else {
 		result = result + "unsuccesful"
 	}
+	log.Println(result)
 	renderFiles("msg", w, result)
 
 }
+func hasBeenAuthenticated(w http.ResponseWriter, r *http.Request) bool {
+	session, _ := store.Get(r, "session_token")
+	a, _ := session.Values["authenticated"]
+
+	if a == nil {
+		return false
+	}
+	return a.(bool)
+
+}
+func storeAuthenticated(w http.ResponseWriter, r *http.Request, v bool) {
+	session, _ := store.Get(r, "session_token")
+	session.Values["authenticated"] = v
+	err := session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func validateUser(username string, password string) bool {
+
 	ctx := context.Background()
 	u, _ := dbQuery.GetUserByName(ctx, username)
 	if u.Username != username {
@@ -104,18 +137,18 @@ func validateUser(username string, password string) bool {
 }
 
 func main() {
-	port := "9004"
+	// port := "9004"
 
-	if value, exists := os.LookupEnv("SERVER_PORT"); exists {
-		port = value
-	}
-	log.Println(port)
+	// if value, exists := os.LookupEnv("SERVER_PORT"); exists {
+	// 	port = value
+	// }
+	// log.Println(port)
 	/*---- Unused Code right now
 	connecting to the database
 			------------------------*/
 	// dbURI := fmt.Sprintf("%s?sslmode=disable",
 	// 	GetAsString("PSQLURL", "Fail"))
-
+	//
 	// db, err := sql.Open("postgres", dbURI)
 	// if err != nil {
 	// 	panic(err)
@@ -158,14 +191,13 @@ func main() {
 	// 	fmt.Println(fmt.Sprintf("Name : %s, ID : %d", usr.Name, usr.Pkuser))
 
 	// }
-
+	initDatabase()
+	// makeTestUser()
 	//router/server setup
 	router := mux.NewRouter()
 
 	//post handler for /login
 	router.HandleFunc("/login", authenticationHandler).Methods("POST")
-	spa := staticHandler{staticPath: "static", indexPage: "index.html"}
-	router.PathPrefix("/").Handler(spa)
 
 	//embed handler for /css path
 	cssContentStatic, _ := fs.Sub(cssEmbed, "css")
@@ -175,10 +207,10 @@ func main() {
 	//embed handler for /app path (still need to create app section)
 	contentStatic, _ := fs.Sub(staticEmbed, "static")
 	static := http.FileServer(http.FS(contentStatic))
-	router.PathPrefix("/app").Handler(http.StripPrefix("/app", static))
+	router.PathPrefix("/app").Handler(securityMiddleware(http.StripPrefix("/app", static)))
 
 	//add /login path
-	router.PathPrefix("/login").Handler(http.StripPrefix("/login", static))
+	router.PathPrefix("/login").Handler(securityMiddleware(http.StripPrefix("/login", static)))
 
 	//root will redirect to /app
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -193,4 +225,37 @@ func main() {
 	}
 	//start the server
 	log.Fatal(srv.ListenAndServe())
+}
+func initDatabase() {
+	log.Println("Initializing Database")
+
+	dbURI := fmt.Sprintf("%s?sslmode=disable",
+		GetAsString("PSQLURL", "Fail"))
+	log.Println(dbURI)
+	db, err := sql.Open("postgres", dbURI)
+	if err != nil {
+		panic(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatalln("Error from database ping:", err)
+	}
+	dbQuery = dbTraxl.New(db)
+
+}
+func makeTestUser() {
+	hashPass, _ := pkg.HashPassword("newPassword")
+	userData := dbTraxl.CreateUsersParams{
+		Username:     "newUser",
+		Passwordhash: hashPass,
+		Name:         "Developer",
+	}
+	ctx := context.Background()
+
+	newUser, err := dbQuery.CreateUsers(ctx, userData)
+	if err != nil {
+		log.Println("error creating test user")
+	} else {
+		log.Println(newUser)
+	}
+
 }
